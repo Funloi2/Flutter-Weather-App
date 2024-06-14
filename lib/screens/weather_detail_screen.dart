9f20c5274f03a33636f2645bf3830e488585ff69
Charles <thefunloi11@gmail.com>
2024-06-14 18:38:52 +0200

    add style to weather details

--- lib/screens/weather_detail_screen.dart
+++ lib/screens/weather_detail_screen.dart
@@ -5,7 +5,8 @@ import '../style/single_weather_template.dart';
 class WeatherDetailScreen extends StatelessWidget {
   final HourlyUnit hourlyUnit;
 
-  const WeatherDetailScreen({Key? key, required this.hourlyUnit}) : super(key: key);
+  const WeatherDetailScreen({Key? key, required this.hourlyUnit})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,28 +16,42 @@ class WeatherDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Weather Details"),
       ),
-      body: Center(
-        child: Card(
-          elevation: 8.0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16.0),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 43, 196, 229),
+              Color.fromARGB(255, 24, 109, 127),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
-          margin: const EdgeInsets.all(30.0),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text(
-                  "Temperature: ${hourlyUnit.temperature2m}°C",
-                  style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "Time: $formattedTime",
-                  style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                // Add more information as needed
-              ],
+        ),
+        child: Center(
+          child: Card(
+            elevation: 8.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            margin: const EdgeInsets.all(30.0),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    "Temperature: ${hourlyUnit.temperature2m}°C",
+                    style: const TextStyle(
+                        fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Time: $formattedTime",
+                    style: const TextStyle(
+                        fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                  // Add more information as needed
+                ],
+              ),
             ),
           ),
         ),
